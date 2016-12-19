eb5d5fa7675ba33c961d78e1590cfab2dad930a4
Kirill Khlopko <k.khlopko@mobidev.biz>
2016-12-19 21:40:23 +0200

    Added `parseChain` experimental implementation. Added documenting comments.

--- Parse.swift
+++ Parse.swift
@@ -24,21 +24,66 @@
 // SOFTWARE.
 //
 
+///
+/// Static-typed parsing of json values.
+///
+/// This function helps to parse value from json to any type of property, that implements `Convertible` protocol.
+/// Types to be parsed easy to add and manage.
+///
+/// - parameter input: Some type to be parsed from.
+///
+/// - returns: Parsed value according to specified `Out` type.
+///
 public func parse<In, Out: Convertible>(_ input: In?) -> Out {
     return Out.cast(input)
 }
 
+///
+/// Static-typed parsing of json array.
+///
+/// - parameter input: Some type to be parsed from.
+/// - parameter map: Transformation closure that will be applyied to all elements in array. Return type is same to `Element` of array.
+///
+/// - returns: Parsed value according to specified `Out` type.
+///
 public func parse<In, Out>(_ input: In?, _ map: (In) -> (Out)) -> [Out] {
     let array: [In] = parse(input)
     let result = array.map(map)
     return result
 }
 
-// MARK: - Convertible
+///
+/// Parses a chain of included dictionaries and returns typed result.
+///
+/// This is experimantal implementation, not tested enoght.
+///
+/// - parameter input: Some json to be parsed from.
+/// - parameter keys: List of keys for inner structures. Can contain any count of keys.
+///
+/// - returns: Parsed value according to specified `Out` type.
+///
+public func parseChain<Out: Convertible>(_ input: JSON, keys: String ...) -> Out {
+    return parse(keys.reduce(input) { result, key in parse(result[key]) })
+}
 
+///
+/// Convertible protocol.
+///
 public protocol Convertible {
+    
+    ///
+    /// Static method that incapsulates casting logic to concrete type.
+    ///
+    /// Even if casting failed, in implementation should be returned default value.
+    ///
+    /// - parameter input: Some type to be parsed from.
+    /// 
+    /// - returns: Instance of class, that implements that protocol.
+    ///
     static func cast<T>(_ input: T) -> Self
 }
+
+/// Here is some default implementations for basic types. You can change it as you need to.
 
 extension String: Convertible {
     
